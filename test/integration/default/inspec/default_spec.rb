9ddb1752b25e43d52e55c3bc14a7dddbc84f1751
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2017-10-09 11:06:09 +0100

    Now installs PLINK v1.90b4.7

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,4 +1,4 @@
-plink_version = 'PLINK v1.90b4.6 64-bit (15 Aug 2017)'
+plink_version = 'PLINK v1.90b4.7 64-bit (7 Oct 2017)'
 
 # Check that plink executable is in the path
 describe command('plink --version | xargs echo -n') do
